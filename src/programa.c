18dc6903eee9db568fe85b6ba7852f12c93067fc
Roymar Castillo <roymarcastillo10@gmail.com>
2026-03-25 19:09:29 -0600

    se actualiza el struct programa.

--- src/programa.c
+++ src/programa.c
@@ -14,17 +14,28 @@ void liberar_programa(Programa *programa) {
     liberar_lista_facturas(&programa->facturas);
 }
 
-
 int guardar_programa(const Programa *programa) {
-    guardar_eventos(&programa->eventos, RUTA_ALMACENAMIENTO_EVENTOS);
-    guardar_sitios(&programa->sitios, RUTA_ALMACENAMIENTO_SITIOS);
-    guardar_facturas(&programa->facturas, RUTA_ALMACENAMIENTO_FACTURAS);
+    if (guardar_sitios(&programa->sitios, RUTA_ALMACENAMIENTO_SITIOS) != OPERACION_EXITOSA) {
+        return OPERACION_FALLIDA;
+    }
+    if (guardar_eventos(&programa->eventos, RUTA_ALMACENAMIENTO_EVENTOS) != OPERACION_EXITOSA) {
+        return OPERACION_FALLIDA;
+    }
+    if (guardar_facturas(&programa->facturas, RUTA_ALMACENAMIENTO_FACTURAS) != OPERACION_EXITOSA) {
+        return OPERACION_FALLIDA;
+    }
     return OPERACION_EXITOSA;
 }
 
 int cargar_programa(Programa *programa) {
-    cargar_sitios(&programa->sitios, RUTA_ALMACENAMIENTO_SITIOS);
-    cargar_eventos(&programa->eventos, RUTA_ALMACENAMIENTO_EVENTOS, &programa->sitios);
-    cargar_facturas(&programa->facturas, RUTA_ALMACENAMIENTO_FACTURAS, &programa->eventos);
+    if (cargar_sitios(&programa->sitios, RUTA_ALMACENAMIENTO_SITIOS) != OPERACION_EXITOSA) {
+        return OPERACION_FALLIDA;
+    }
+    if (cargar_eventos(&programa->eventos, &programa->sitios, RUTA_ALMACENAMIENTO_EVENTOS) != OPERACION_EXITOSA) {
+        return OPERACION_FALLIDA;
+    }
+    if (cargar_facturas(&programa->facturas, &programa->eventos, RUTA_ALMACENAMIENTO_FACTURAS) != OPERACION_EXITOSA) {
+        return OPERACION_FALLIDA;
+    }
     return OPERACION_EXITOSA;
 }
