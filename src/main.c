440e9b008ac702be927784d07c80bfca5121ab19
SidneySJk <andoglobina2727@gmail.com>
2026-03-28 07:50:09 -0600

    Se actualizo la consola
    Se actualizo el menu de gestion de sitios de eventos, ahora se puede visualizar todos los sitios guardados en el sistema

--- src/main.c
+++ src/main.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "asiento.h"
-#include "cargar_guardar.h"
-#include "config.h"
-#include "evento.h"
-#include "factura.h"
-#include "fecha.h"
-#include "lista_asientos.h"
-#include "lista_eventos.h"
-#include "lista_sectores.h"
-#include "lista_facturas.h"
-#include "lista_sitios.h"
-#include "programa.h"
-#include "sector.h"
-#include "sitio.h"
-#include "usuario.h"
+#include "../include/cargar_guardar.h"
+#include "../include/config.h"
+#include "../include/evento.h"
+#include "../include/factura.h"
+#include "../include/fecha.h"
+#include "../include/lista_asientos.h"
+#include "../include/lista_eventos.h"
+#include "../include/lista_sectores.h"
+#include "../include/lista_facturas.h"
+#include "../include/lista_sitios.h"
+#include "../include/programa.h"
+#include "../include/sector.h"
+#include "../include/sitio.h"
+#include "../include/usuario.h"
 
 void inicioSesion();
 void menu();
@@ -26,11 +25,14 @@ void menuEE();
 void menuLF();
 
 int main() {
-    inicioSesion();
+    ListaSitios lista_sitios = crear_lista_sitios();
+    menu(lista_sitios);
+    inicioSesion(lista_sitios);
+    liberar_lista_sitios(&lista_sitios);
     return 0;
 }
 
-void menu() {
+void menu(ListaSitios lista_sitios) {
     printf("¡Gestion de eventos!\n");
     printf("Ingrese una opcion: \n");
     printf("1. Gestión de Sitios de Eventos\n");
@@ -44,7 +46,7 @@ void menu() {
     scanf("%d", &opc);
     switch(opc) {
         case 1:
-            menuGSE();
+            menuGSE(&lista_sitios);
             break;
         case 2:
             menuGESE();
@@ -59,12 +61,12 @@ void menu() {
             menuLF();
             break;
         case 6:
-            inicioSesion();
+            inicioSesion(lista_sitios);
             break;
     }
 }
 
-void inicioSesion() {
+void inicioSesion(ListaSitios lista_sitios) {
     FILE *arch;
     arch = fopen("Usuarios.txt", "w");
     //if (arch == NULL) {
@@ -84,68 +86,64 @@ void inicioSesion() {
     fputs(linea, arch);
 
     if (strlen(usuario) > 0 && strlen(contrasena) > 0) {
-        menu();
+        menu(lista_sitios);
 
     } else {
         printf("Contraseña o Usuario vacio, ingrese denuevo");
-        inicioSesion();
+        inicioSesion(lista_sitios);
 
     }
+     
     free(usuario);
     free(contrasena);
     free(linea);
     fclose(arch);
+    
 }
 
-void menuGSE() {
-    printf("Carga en lote\n");
+void menuGSE(ListaSitios *lista_sitios) {
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("            Carga en lote\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("Indique la ruta del archivo:\n");
     
     char *ruta = (char*) malloc(256 * sizeof(char));
     if (ruta == NULL) return;
-    char **array = (char**) malloc(10 * sizeof(char*));
-    if (array == NULL) { free(ruta); return;}
     char *linea = (char*) malloc(50 * sizeof(char));
-    if (linea == NULL) { free(ruta); free(array); return;}
+    if (linea == NULL) { free(ruta); return;}
     
     FILE *arch = NULL; 
+    scanf("%255s", ruta);
+    arch = fopen(ruta, "r");
 
     while (!arch) {
+        printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
         printf("Ruta no encontrada, intente denuevo...\n");
+        printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
         scanf("%255s", ruta);
         arch = fopen(ruta, "r");
-        if (!arch) printf ("No trunca");
-        printf("[%s]\n", ruta);
     }
-
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("Ruta hallada, extrayendo informacion...\n");
-    int i = 0;
-    while (fgets(linea, 50, arch) && i < 10) {
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    while (fgets(linea, 50, arch)) {
         linea[strcspn(linea, "\n")] = '\0';
-        char *cursor = strtok(linea, ",");
         
-        while (cursor != NULL && i < 10) {
-            array[i] = strdup(cursor);
-            cursor = strtok(NULL, ",");
-            i++;
+        char *nombre = strtok(linea, ",");
+        char *ubicacion = strtok(NULL, ",");
+        char *sitio_web = strtok(NULL, ",");
+
+        if(nombre && ubicacion){
+            Sitio sitio = crear_sitio(nombre, ubicacion, sitio_web);
+            agregar_sitio(lista_sitios, &sitio);
         }
     }
-
-    fclose(arch);
     
-    Sitio sitio;
-    sitio.nombre = array[0];
-    sitio.ubicacion = array[1];
-    sitio.sitio_web = array[2];
-
-    for (int fila = 0; fila < 10; fila++) {
-        free(array[fila]);
-    }
-
+    fclose(arch);
+    mostrar_lista_sitios(lista_sitios);
     free(ruta);
-    free(array);
     free(linea);
-    menu();
+
 }
 
 void menuGESE() {
